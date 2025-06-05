bbf76689b2ec931905ed391f4eeb8d797b98eaf9
IvanMazhara <s31385@pjwstk.edu.pl>
2025-06-06 00:04:37 +0200

    ClientRequestDto update

--- apbd-12/Models/DTOs/ClientTripRequestDto.cs
+++ apbd-12/Models/DTOs/ClientTripRequestDto.cs
@@ -2,10 +2,10 @@ namespace apbd_12.Models.DTOs;
 
 public class ClientTripRequestDto
 {
-    public int IdClient { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public string Email { get; set; }
     public string Telephone { get; set; }
     public string Pesel { get; set; }
+    public DateTime? PaymentDate { get; set; }
 }
