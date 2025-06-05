f1a0d87cc15c37d4f885d31a1015c4d4707af9cb
IvanMazhara <s31385@pjwstk.edu.pl>
2025-06-06 00:04:05 +0200

    Controllers final update

--- apbd-12/Controllers/ClientsController.cs
+++ apbd-12/Controllers/ClientsController.cs
@@ -1,10 +1,35 @@
+using apbd_12.Repositories;
 using Microsoft.AspNetCore.Mvc;
 
 namespace apbd_12.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class ClientsController
+public class ClientsController : ControllerBase
 {
-    
+    private readonly IDbService _dbService;
+
+    public ClientsController(IDbService dbService)
+    {
+        _dbService = dbService;
+    }
+
+    [HttpDelete]
+    [Route("{idClient}")]
+    public async Task<IActionResult> DeleteClientById(int idClient)
+    {
+        try
+        {
+            var result = await _dbService.DeleteClientById(idClient);
+            return Ok($"Client with ID {idClient} has been deleted.");
+        }
+        catch (InvalidOperationException ex)
+        {
+            return Conflict(new { message = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            return NotFound(new { message = ex.Message });
+        }
+    }
 }
--- apbd-12/Controllers/TripsController.cs
+++ apbd-12/Controllers/TripsController.cs
@@ -1,12 +1,13 @@
 using apbd_12.Models.DTOs;
 using apbd_12.Repositories;
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 
 namespace apbd_12.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
-public class TripsController
+public class TripsController : ControllerBase
 {
     private readonly IDbService _dbService;
 
@@ -16,9 +17,28 @@ public class TripsController
     }
 
     [HttpGet]
-    public async Task<TripResponseDto> GetTrip(int id)
+    public async Task<List<TripResponseDto>> GetTrips()
     {
-        var trips = await _dbService.GetAllTrips();
-        return trips.FirstOrDefault();
+        return await _dbService.GetAllTrips();
     }
+
+    [HttpPost]
+    [Route("{idTrip}/clients")]
+    public async Task<IActionResult> AssignClientToTrip(int idTrip, [FromBody] ClientTripRequestDto request)
+    {
+        try
+        {
+            await _dbService.AssignClientToTrip(idTrip, request);
+            return Ok("Client assigned to the trip.");
+        }
+        catch (InvalidOperationException ex)
+        {
+            return Conflict(new { message = ex.Message });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new { message = ex.Message });
+        }
+    }
+
 }
