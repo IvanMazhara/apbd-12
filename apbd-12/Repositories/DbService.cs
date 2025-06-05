20f619f2f505dce8f4dd44eb4c5cb4b1fc90c4f5
IvanMazhara <s31385@pjwstk.edu.pl>
2025-06-05 16:20:03 +0200

    Repository/interface update

--- apbd-12/Repositories/DbService.cs
+++ apbd-12/Repositories/DbService.cs
@@ -1,6 +1,20 @@
-﻿namespace apbd_12.Repositories;
+﻿using apbd_12.Data;
+using apbd_12.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace apbd_12.Repositories;
 
 public class DbService : IDbService
 {
-    
+    private readonly DatabaseContext _context;
+
+    public DbService(DatabaseContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<List<Trip>> GetAllTripsAsync()
+    {
+        return await _context.Trips.ToListAsync();
+    }
 }
--- apbd-12/Repositories/IDbService.cs
+++ apbd-12/Repositories/IDbService.cs
@@ -1,6 +1,8 @@
-﻿namespace apbd_12.Repositories;
+﻿using apbd_12.Models;
+
+namespace apbd_12.Repositories;
 
 public interface IDbService
 {
-    
+    Task<List<Trip>> GetAllTripsAsync();
 }
